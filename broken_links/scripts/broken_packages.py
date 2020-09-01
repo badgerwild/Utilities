a34f5b0f998403a5a0287b0c8191af62cc3fb9d0
badgerwild <jklimock@gmail.com>
2020-09-01 18:07:16 -0400

    Add funtion to get and trim ppa urls to be easily used with ppa-purge tool

--- broken_links/scripts/broken_packages.py
+++ broken_links/scripts/broken_packages.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 import sys
+import os
+import re
 from pprint import pprint
+FAIL = '/var/log/dist-upgrade/apt.log'
+SOURCE = '/etc/apt/sources.list.d'
+
 def get_broken_packages(file_name):
     ppa_list = []
     with open(file_name) as file:
@@ -10,10 +15,21 @@ def get_broken_packages(file_name):
     if not ppa_list:
         return "list is empty"
     else:
+        ppa_list.sort()
         return ppa_list
 
-#ef remove_packages():
-
+def get_ppas(dir_name):
+    ppa_source = []
+    for dirpath, dirnames, filename in os.walk(dir_name):
+        print(dirpath)
+        for files in filename:
+             with open(dirpath + "/" + files) as f:
+                for line in f.readlines():
+                    str_process = ''.join(re.findall('https?.*', ''.join(line).strip()))
+                    ppa_source.append(str_process.split(" ")[0])
+    return set(ppa_source)
 
 if __name__ == "__main__":
-    pprint(get_broken_packages(sys.argv[1]))
+    print(get_broken_packages(FAIL))
+    print('********************************************************************')
+    print(get_ppas(SOURCE))
